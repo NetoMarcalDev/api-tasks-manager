eacb4acf4501f1ee961e8dbfdf0ba1ac6bde36d6
Neto <neto.marcal.dev@gmail.com>
2023-09-16 18:36:42 -0300

    Implementação da feature createTask.

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,15 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task } from './entities/entity.task';
 @Controller('task')
 export class TaskController {
     constructor(private taskService: TaskService) {}
+
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
+        return this.taskService.createTask(createTaskDto);
+    }
+
 }
