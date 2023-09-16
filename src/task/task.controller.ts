49d29d899be06fdea66850590ab2565d2fadc70a
Neto <neto.marcal.dev@gmail.com>
2023-09-16 18:32:13 -0300

    Injeçao de dependência do TaskService no controller.

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,7 @@
+/* eslint-disable prettier/prettier */
 import { Controller } from '@nestjs/common';
-
+import { TaskService } from './task.service';
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+    constructor(private taskService: TaskService) {}
+}
