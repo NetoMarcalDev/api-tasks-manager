d2b82cf401b780b3f159f786c8ef246cae00d942
Neto <neto.marcal.dev@gmail.com>
2023-09-16 16:01:56 -0300

    injeção de dependência TasksRepository no dervice task.

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { TasksRepository } from './repositories/tasks.repository';
 @Injectable()
-export class TaskService {}
+export class TaskService {
+    constructor(
+        @InjectRepository(TasksRepository)
+        private tasksRepository: TasksRepository,
+    ) {}
+    
+}
