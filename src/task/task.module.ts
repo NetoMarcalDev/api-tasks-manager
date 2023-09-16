46e75d125b2b74790138ca50418d5be5a9a283a0
Neto <neto.marcal.dev@gmail.com>
2023-09-16 15:48:26 -0300

    Importação do TasksRepository

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksRepository } from './repositories/tasks.repository';
 @Module({
+  imports: [TypeOrmModule.forFeature([TasksRepository])],
   controllers: [TaskController],
-  providers: [TaskService]
+  providers: [TaskService],
 })
 export class TaskModule {}
